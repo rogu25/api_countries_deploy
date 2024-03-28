b179c6bbb3a86735d28ffd72bd2ec62f55213856
rogu25 <ryavar.montesinos@gmail.com>
2024-03-27 20:27:33 -0500

    probando deployar

--- index.js
+++ index.js
@@ -20,9 +20,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+require('dotenv').config();
+
+const port = process.env.PORT || 3001; // puerto por defecto postgres
+
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
--- src/db.js
+++ src/db.js
@@ -3,10 +3,11 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE
 } = process.env;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?ssl=true`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectModule: require('pg')
